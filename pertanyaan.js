35e02db1b0e909a1a4a162628ed1efa4d805349c
fajarsyah <angkasarayanet@gmail.com>
2019-02-26 21:36:05 +0700

    update file js

--- pertanyaan.js
+++ pertanyaan.js
@@ -1,43 +1,44 @@
-var soal = [
+  var DaftarSoal = [
     {
-      question: "1. Jika diperlukan waktu satu menit untuk merebus satu telur, berapa waktu yang diperlukan untuk merebus sepuluh telur?",
-      option1: "A. 10 Menit",
-      option2: "C. 1 Menit",
-      option3: "B. 15 Menit",
-      option4: "D. 2 Menit",
-      answer: 2
+      soal:"1. Apakah Kepanjangan Dari HTML?",
+      jawaban1:"A. Hyper Text Markup Language",
+      jawaban2:"C. Hyperlinks Text Markup Language",
+      jawaban3:"B. Hyperlinks Markup Language",
+      jawaban4:"D. Hyper Markup Language",
+      jawabanAsli: 1
     },
     {
-      question: "2. Berapa kali angka 7 muncul di antara bilangan 1 sampai 100?",
-      option1: "18 kali",
-      option2: "19 kali",
-      option3: "20 kali",
-      option4: "21 kali",
-      answer: 3
+      soal:"2. Bagaimana Cara Membuat Baris Baru?",
+      jawaban1:"A. <breakline>",
+      jawaban2:"C. <br>",
+      jawaban3:"B. <break>",
+      jawaban4:"D. <newline>",
+      jawabanAsli: 2
     },
     {
-      question: "3. A bepergian dengan mobil mulai dari kota P ke kota Q dengan kecepatan tetap 75 km/jam. B juga bepergian dengan mobil mulai dari kota Q ke kota P dengan kecepatan tetap 50 km/jam. Jika A dan B berangkat pada waktu yang bersamaan, saat A dan B bertemu, siapa yang lebih dekat dengan kota P?",
-      option1: "A",
-      option2: "B",
-      option3: "Sama dekat",
-      option4: "P",
-      answer: 3
+      soal:"3. Bagaimana Cara Membuat Paragraft?",
+      jawaban1:"A. <paragraph>",
+      jawaban2:"C. <paragraf>",
+      jawaban3:"B. <p>",
+      jawaban4:"D. <pr>",
+      jawabanAsli: 3
+
     },
     {
-      question: "4. Mana yang lebih besar, 18 persen dari 81 atau 81 persen dari 18?",
-      option1: "81 persen dari 18",
-      option2: "18 persen dari 81",
-      option3: "81 persen dari 18",
-      option4: "Sama besar",
-      answer: 4
+      soal:"4. Bagaimana cara membuat heading di dalam halaman HTML?",
+      jawaban1:"A. <h1>",
+      jawaban2:"C. <header>",
+      jawaban3:"B. <title>",
+      jawaban4:"D. <tl>",
+      jawabanAsli: 1
     },
     {
-      question: "5. A ingin pergi dengan mobil dari kota P ke kota Q dengan kecepatan rata-rata 60 km/jam. Ternyata, setengah perjalanan pertama ditempuh A dengan kecepatan rata-rata 30 km/jam. Berapa kecepatan rata-rata yang harus dicapai A saat dia menempuh setengah perjalanan sisanya sehingga pada akhirnya dia dapat mencapai kecepatan rata-rata keseluruhan 60 km/jam?",
-      option1: "A tidak mungkin bisa",
-      option2: "120 km/jam",
-      option3: "90 km/jam",
-      option4: "60 km/jam",
-      answer: 1
+      soal:"5. Bagaimana cara membuat background pada element HTML?",
+      jawaban1:'A. <body background="red">',
+      jawaban2:'C. <body bg="red">',
+      jawaban3:'B. <body background-color="red">',
+      jawaban4:'D. <body style="background:red"',
+      jawabanAsli: 4
     }
   ]
 
@@ -60,6 +61,12 @@ var soal = [
     stopCount();
    // alert("waktu habis")
    document.getElementById("durasi").innerHTML = "Waktu Anda Habis"
+   if(document.getElementById("durasi").innerHTML = "Waktu Anda Habis" && totalPertanyaan === 0){
+    blockPertanyaan();
+    document.getElementById("durasi").innerHTML = " Waktu Anda Habis"
+    document.getElementById("hasilAkhirPoint").innerHTML = " Dan Anda Belum Menjawab Pertanyaan Satupun"
+
+  }
   }
   }
   
@@ -84,12 +91,11 @@ var soal = [
 
   
 var point = 0
-var answer = 0
 namaPeserta = ''
 
-//Option
-var currentQuestion = 0
-var totalQuestion = soal.length
+totalPertanyaan = 0
+
+var urutanSoal = 0
 var totalJawabBenar = 0
 var totalJawabSalah = 0
 
@@ -103,38 +109,38 @@ var buttonLagi = document.getElementById("buttonMulaiLagi")
 
 var hilanginTampilan = document.getElementById("questionContainer")
 
-var question = document.getElementById("pertanyaannya")
-op1.addEventListener("click", clickOption1);
-op2.addEventListener("click", clickOption2);
-op3.addEventListener("click", clickOption3);
-op4.addEventListener("click", clickOption4);
+var soal = document.getElementById("pertanyaannya")
+op1.addEventListener("click", pilihJawaban1);
+op2.addEventListener("click", pilihJawaban2);
+op3.addEventListener("click", pilihJawaban3);
+op4.addEventListener("click", pilihJawaban4);
 
-function clickOption1(){
-  answer = 1
+function pilihJawaban1(){
+  jawabanAsli = 1
   op1.style.background = "rgb(24, 63, 102)"
   op2.style.background = "rgb(109, 164, 221)"
   op3.style.background = "rgb(109, 164, 221)"
   op4.style.background = "rgb(109, 164, 221)"
 }
 
-function clickOption2(){
-  answer = 2
+function pilihJawaban2(){
+  jawabanAsli = 2
   op1.style.background = "rgb(109, 164, 221)"
   op2.style.background = "rgb(24, 63, 102)"
   op3.style.background = "rgb(109, 164, 221)"
   op4.style.background = "rgb(109, 164, 221)"
 }
 
-function clickOption3(){
-  answer = 3
+function pilihJawaban3(){
+  jawabanAsli = 3
   op1.style.background = "rgb(109, 164, 221)"
   op2.style.background = "rgb(109, 164, 221)"
   op3.style.background = "rgb(24, 63, 102)"
   op4.style.background = "rgb(109, 164, 221)"
 }
 
-function clickOption4(){
-  answer = 4
+function pilihJawaban4(){
+  jawabanAsli = 4
   op1.style.background = "rgb(109, 164, 221)"
   op2.style.background = "rgb(109, 164, 221)"
   op3.style.background = "rgb(109, 164, 221)"
@@ -142,22 +148,24 @@ function clickOption4(){
 }
 
 function resetButton(){
-  answer = 0
+  jawabanAsli = 0
   op1.style.background = "rgb(0, 0, 255)"
   op2.style.background = "rgb(0, 0, 255)"
   op3.style.background = "rgb(0, 0, 255)"
   op4.style.background = "rgb(0, 0, 255)"
 }
-  function loadQuestion(){
-   document.getElementById("judul").innerHTML = 'Ayo semangat '+ namaPeserta + '. Kamu Pasti Bisa!'
-    //buttonLagi.style.display = "none"
+  function tampilkanSoalnya(){
+   document.getElementById("judul").innerHTML = 'Ayo Semangat '+ namaPeserta + '. Kamu Pasti Bisa!'
     judulNama = namaPeserta;
-    var ques = soal[currentQuestion]
-    question.textContent = ques.question;
-    op1.textContent = ques.option1;
-    op2.textContent = ques.option2;
-    op3.textContent = ques.option3;
-    op4.textContent = ques.option4;
+    var ques = DaftarSoal[urutanSoal]
+    soal.textContent = ques.soal;
+    op1.textContent = ques.jawaban1;
+    op2.textContent = ques.jawaban2;
+    op3.textContent = ques.jawaban3;
+    op4.textContent = ques.jawaban4;
+    // var totalDurasiku = 59 - c
+    
+    
     
   }
 
@@ -169,22 +177,14 @@ function resetButton(){
   
   function result(){
     
-    if (point >= 90) {
+    if (point >= 70) {
      
-      alert('anda pintar')
+      alert('Selamat Anda Menang')
       
+    }else{
+      alert('Maaf Anda Gagal')
     }
-    else if (point <= 80 && point >= 60) {
-     
-      alert('anda cukup')
-    }
-    else if (point <=50 && point >= 20) {
-      
-    }
-    else if (point < 20) {
-   
-    alert('dedel')
-  }
+    
   stopCount()
   var totalDurasiku = 59 - c
   
@@ -195,59 +195,54 @@ function resetButton(){
   document.getElementById("hasilAkhirPoint").innerHTML = namaPeserta +' Mendapatkan : ' + point + ' point';
   document.getElementById("jawabBenar").innerHTML = "Total Jawaban Benar " +namaPeserta+ " : " + totalJawabBenar + " pertanyaan"
   document.getElementById("jawabSalah").innerHTML = "Total Jawaban Salah " +namaPeserta+ " : " + totalJawabSalah + " pertanyaan"
-console.log(namaPeserta)
-//buttonLagi.style.display =""
 
-
-
-  // if(point >= 90){
-  //   document.getElementById("ucapanAkhir").innerHTML = "Selamat Anda Berhasil"
-  // }
 }
-  
 
-
-  function loadNextQuestion(){
-    if (currentQuestion === soal.length-1) {
-      if (answer === soal[currentQuestion].answer) {
+  function kirimJawabannya(){
+    if (urutanSoal === DaftarSoal.length-1) {
+      if (jawabanAsli === DaftarSoal[urutanSoal].jawabanAsli) {
         point+=20
         totalJawabBenar ++
+        totalPertanyaan ++
         console.log(point)
         
       }
       else {
         point-=10
         totalJawabSalah ++
+        totalPertanyaan ++
         console.log(point)
         
       }
       result()
       return
     }
-    if (answer === 0) {
+    if (jawabanAsli === 0) {
       alert("Kamu belum pilih jawaban, silahkan pilih jawaban terlebih dahulu")
       return
     }
-    if (answer === soal[currentQuestion].answer) {
+    if (jawabanAsli === DaftarSoal[urutanSoal].jawabanAsli) {
    
     point +=20
     totalJawabBenar ++
+    totalPertanyaan ++
     console.log(point)
     
     }
-    else if (answer !== soal[currentQuestion].answer) {
+    else if (jawabanAsli !== DaftarSoal[urutanSoal].jawabanAsli) {
       point-=10
       totalJawabSalah ++
+      totalPertanyaan ++
       console.log(point)
     
     }
-    currentQuestion++
-    loadQuestion(currentQuestion)
+    urutanSoal++
+    tampilkanSoalnya(urutanSoal)
     resetButton()
   }
   
   
   var namaPeserta = prompt("Masukkan Nama Anda!")
-  loadQuestion()
+  tampilkanSoalnya()
   
   
